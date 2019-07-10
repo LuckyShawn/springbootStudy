4be3e9694b4e7da3fb2a19950504bd79cd21d637
luckyshawn <luckysahwn@qq.com>
2019-07-10 09:55:57 +0800

    spring+thymeleaf+layui

--- src/main/java/com/shawn/springboot/controller/ThymeleafController.java
+++ src/main/java/com/shawn/springboot/controller/ThymeleafController.java
@@ -1,7 +1,6 @@
 package com.shawn.springboot.controller;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
